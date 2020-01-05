97d0a221a8cc554861463a882bb647d9e4a642b8
devOffice <devOffice@bit-tax.com>
2020-01-05 13:14:30 +0200

    Add max and Min Temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location, try again!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. There is a '+ body.currently.precipProbability +'% chance of rain' )
+            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature +' degrees out. The high today is '+ body.daily.data[0].temperatureMax+' with a low of '+ body.daily.data[0].temperatureMin +'. There is a '+ body.currently.precipProbability +'% chance of rain' )
         }
     })
 }
